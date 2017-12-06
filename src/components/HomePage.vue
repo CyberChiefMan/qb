9cad810e8bf97d050df1282e6ca9896a937a69bd
m <mahmoudi.m2@gmail.com>
2017-12-07 00:40:51 +0330

    13960916

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="HomePage">
-      <h1>{{ttt}}</h1>
+
       <h1>{{user.firstName}} is a {{user.lastName}}</h1>
      <!-- <p v-if="showName" style="color:firebrick">{{user.firstName}} </p>
       <p v-if="!showName" style="color:darkblue">{{user.lastName}} </p>-->
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,9 @@
   <div id="Navbar">
 
     <ul>
-      <li v-for="item in items"><a v-html="item.link"></a></li>
-    </ul><router-view></router-view>
+      <li v-for="item in items"><a v-html="item.link">"item.link"</a></li>
+    </ul>
+    <router-view></router-view>
 
 
   </div>
@@ -14,17 +15,18 @@
     name: 'Navbar',
     data(){
       return {
-        items:[
-          {title:'Home',
-           link:'<router-link to="/">HomePage</router-link>'
+        items: [
+          {
+            title: 'Home',
+            link: '<router-link to="/">HomePage</router-link>'
           },
-          {title:'Report',
-            link:'<router-link to="/">ReportPage</router-link>'},
-
-
+          {
+            title: 'Report',
+            link: '<router-link to="/">ReportPage</router-link>'
+          },
         ],
 
-    }
+      }
     }
   }
 </script>
